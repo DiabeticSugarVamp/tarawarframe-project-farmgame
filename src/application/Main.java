d2ebcb7dd00f6d4ee0fee99986cb0d5162f88f3b
DiabeticSugarVamp <israel.rodriguez.dev@gmail.com>
2024-05-09 23:42:05 +0800

    Initial UI Files

---
+++ src/application/Main.java
@@ -0,0 +1,41 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.media.Media;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("IntroDevelopers.fxml"));
+			Scene scene = new Scene(root,800,600);
+			
+			String css = this.getClass().getResource("application.css").toExternalForm();
+			
+			
+			
+			String iconPath = "/assets/icon.jpg";
+			Image icon = new Image(getClass().getResource(iconPath).toString());
+			stage.getIcons().add(icon);
+			
+			stage.setTitle("Plow to Profit");
+			stage.setResizable(false);
+			scene.getStylesheets().add(css);
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ src/application/SceneController.java
@@ -0,0 +1,89 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.stage.Stage;
+
+public class SceneController {
+	@FXML
+	private MediaView mainMenuMusic;
+	
+	@FXML
+	private Button exitPromptButton;
+	
+	@FXML
+	private AnchorPane mainMenuPane;
+	
+	private File file;
+	private Media media;
+	private MediaPlayer mediaPlayer;
+	
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchToSceneMainMenu(MouseEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("mainMenu.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		this.initializeMainMenuMusic();
+		stage.show();
+	}
+	
+	public void switchToSceneCredits(MouseEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("MainMenuCreditsScene.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void exitPrompt(MouseEvent event) {
+		Alert alert = new Alert(AlertType.CONFIRMATION);
+		alert.setTitle("exit");
+		alert.setHeaderText("You are about to exit!");
+		alert.setContentText("Are you sure you want to exit the game?");
+		
+		if (alert.showAndWait().get() == ButtonType.OK) {
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.close();
+		}
+	}
+	
+	public void toggleFullscreen(MouseEvent event) {
+		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		
+		if (!stage.isFullScreen()) {
+			stage.setFullScreen(true);
+		} else {
+			stage.setFullScreen(false);
+		}
+		
+	}
+	
+	public void initializeMainMenuMusic() {
+		String mediaPath = "/assets/Effervescence.mp3";
+		Media media = new Media(getClass().getResource(mediaPath).toString());
+		mediaPlayer = new MediaPlayer(media);
+		mediaPlayer.play();
+	}
+	
+	
+}
