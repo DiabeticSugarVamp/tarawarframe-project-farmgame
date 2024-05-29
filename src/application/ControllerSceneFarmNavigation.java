804cc544ddeb2a3ce205ffa307ed1d28417ef5c1
THIS IS CARL! <139425034+Scarlett663@users.noreply.github.com>
2024-05-29 11:16:52 +0800

    update

--- src/application/ControllerSceneFarmNavigation.java
+++ src/application/ControllerSceneFarmNavigation.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -11,77 +12,85 @@ import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
-public class ControllerSceneFarmNavigation {
-	private Stage stage;
-	private Scene scene;
-	private Parent root;
-	
-	private int currentDay;
-	private int money;
-	private int actionPointsRemaining;
-	private int actionPointsTotal;
-	private int deadline;
-	
-	@FXML
-	Label labelCurrentDay;
-	@FXML
-	Label labelActionPoints;
-	@FXML
-	Label labelMoney;
-	@FXML
-	Label labelDeadline;
-	
-	public void endDay(MouseEvent e) {
-		currentDay++;
-		actionPointsRemaining = actionPointsTotal;
-		deadline -= 1;
-		this.deadline();
-		this.setTopTexts();
-	}
-	
-	public void setTopTexts() {
-		labelCurrentDay.setText("Day: " + currentDay);
-		labelActionPoints.setText("Actions: " + actionPointsRemaining);
-		labelMoney.setText("Money: " + money);
-		labelDeadline.setText("Deadline: " + deadline);
-	}
-	
-	public void deadline() {
-		if (deadline == 0) {
-			money -= 50;
-			deadline += 14;
-		}
-	}
-	
-	public void switchToScenePauseMenu(MouseEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("ScenePauseMenu.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
-	
-	public void switchToSceneFarmFields(MouseEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("SceneFarmFields.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
-	
-	public void switchToSceneFarmAssets(MouseEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("SceneFarmAssets.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
-	
-	public void switchToSceneGuild(MouseEvent event) throws IOException {
-		root = FXMLLoader.load(getClass().getResource("SceneGuild.fxml"));
-		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-		scene = new Scene(root);
-		stage.setScene(scene);
-		stage.show();
-	}
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerSceneFarmNavigation implements Initializable {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    private int currentDay = 1; 
+    private int money = 100; 
+    private int actionPointsRemaining = 10; 
+    private int actionPointsTotal = 10; 
+    private int deadline = 14;
+
+    @FXML
+    private Label labelCurrentDay;
+    @FXML
+    private Label labelActionPoints;
+    @FXML
+    private Label labelMoney;
+    @FXML
+    private Label labelDeadline;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        setTopTexts(); 
+    }
+
+    public void endDay(MouseEvent e) {
+        currentDay++;
+        actionPointsRemaining = actionPointsTotal;
+        deadline -= 1;
+        this.deadline();
+        this.setTopTexts();
+    }
+
+    public void setTopTexts() {
+        labelCurrentDay.setText("Day: " + currentDay);
+        labelActionPoints.setText("Actions: " + actionPointsRemaining);
+        labelMoney.setText("Money: " + money);
+        labelDeadline.setText("Deadline: " + deadline);
+    }
+
+    public void deadline() {
+        if (deadline == 0) {
+            money -= 50;
+            deadline += 14;
+        }
+    }
+
+    public void switchToScenePauseMenu(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("ScenePauseMenu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSceneFarmFields(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("SceneFarmFields.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSceneFarmAssets(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("SceneFarmAssets.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSceneGuild(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("SceneGuild.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
