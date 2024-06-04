45993c419a84634bd219d44508ccdb2a62206472
DiabeticSugarVamp <israel.rodriguez.dev@gmail.com>
2024-06-04 13:02:27 +0800

    - Updated credits and added win and lose scenes.

---
+++ src/application/ControllerSceneEndLose.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class ControllerSceneEndLose {
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	
+	public void switchToSceneMainMenu(MouseEvent event) throws IOException {
+		root = FXMLLoader.load(getClass().getResource("SceneMainMenu.fxml"));
+		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+		scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+	}
+}
